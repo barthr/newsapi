f3125ec6b92607f9e5e4f1075fd3d9bde136366e
Bart Fokker <bart@publicsonar.com>
2017-06-28 18:15:13 +0200

    small updates

--- client.go
+++ client.go
@@ -3,7 +3,6 @@ package newsapi
 import (
 	"encoding/json"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 
@@ -26,41 +25,57 @@ type Client struct {
 	UserAgent string
 }
 
-func NewClient(apiKey string, httpClient *http.Client) *Client {
-	if httpClient == nil {
-		httpClient = http.DefaultClient
-	}
+type OptionFunc func(*Client)
 
-	baseURL, _ := url.Parse(defaultBaseURL)
-
-	return &Client{
-		client:  httpClient,
-		apiKey:  apiKey,
-		BaseURL: baseURL,
+func WithHttpClient(client *http.Client) OptionFunc {
+	return func(c *Client) {
+		c.client = client
 	}
 }
 
-func (c *Client) NewGetRequest(urlStr string) (*http.Request, error) {
-	rel, err := url.Parse(urlStr)
+// NewClient returns a new newsapi client to query the newsapi API.
+func NewClient(apiKey string, options ...OptionFunc) *Client {
+	baseURL, _ := url.Parse(defaultBaseURL)
+	c := &Client{
+		BaseURL: baseURL,
+		apiKey:  apiKey,
+	}
+
+	for _, opt := range options {
+		opt(c)
+	}
+	return c
+}
+
+// NewGetRequest returns a new Get request for the given url URLStr
+// It returns a pointer to a http request which can be executed by a http.client
+func (c *Client) NewGetRequest(URLStr string) (*http.Request, error) {
+	rel, err := url.Parse(URLStr)
 	if err != nil {
 		return nil, err
 	}
 
 	u := c.BaseURL.ResolveReference(rel)
 
+	// Create a new get request with the url provided
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
 
+	// Set the api key on the request
 	req.Header.Set(apiKeyHeader, c.apiKey)
 
+	// If we specify a user agent we override the current one
 	if c.UserAgent != "" {
 		req.Header.Set("User-Agent", c.UserAgent)
 	}
 	return req, nil
 }
 
+// Do executes the http.Request and marshal's the response into v
+// v must be a pointer to a value instead of a regular value
+// It returns the actual response from the request and also an error
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.client.Do(req)
 
@@ -68,12 +83,7 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 		return resp, err
 	}
 
-	defer func() {
-		// Drain up to 512 bytes and close the body to let the Transport reuse the connection
-		// Found in https://github.com/google/go-github
-		io.CopyN(ioutil.Discard, resp.Body, 512)
-		resp.Body.Close()
-	}()
+	defer resp.Body.Close()
 
 	err = json.NewDecoder(resp.Body).Decode(v)
 	if err == io.EOF {
@@ -83,15 +93,17 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	return resp, err
 }
 
-func setOptions(reqUrl string, opts interface{}) (string, error) {
-	u, err := url.Parse(reqUrl)
+// setOptions set the options for the url
+// It will set the query parameters and encodes them with the url
+func setOptions(reqURL string, options interface{}) (string, error) {
+	u, err := url.Parse(reqURL)
 	if err != nil {
-		return reqUrl, err
+		return reqURL, err
 	}
 
-	qs, err := query.Values(opts)
+	qs, err := query.Values(options)
 	if err != nil {
-		return reqUrl, err
+		return reqURL, err
 	}
 
 	u.RawQuery = qs.Encode()
